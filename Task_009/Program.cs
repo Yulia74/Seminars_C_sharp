3b0f70ac8d8bba93feff7674eb561f5671b932ee
[Yulia Lebedeva] <yl.75@mail.ru>
2022-11-28 23:07:57 +0500

    Task 009 is done

---
+++ Task_009/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа:
+// например: 78 -> 8,  12 -> 2,     85 ->8
+
+Random random = new Random();
+int number = random.Next(10,100);
+Console.WriteLine(number);
+
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+if(firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(lastNumber);
+}
