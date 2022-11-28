4b201d27a82800773afb7f7e2710d184c12fd7e2
[Yulia Lebedeva] <yl.75@mail.ru>
2022-11-28 22:51:35 +0500

    Task 001 is done

---
+++ Task_001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго
+// a=25, b=5 -> да;   a=2, b=10 -> нет;    a=9, b=-3 -> да; // a=-3, b=9 -> нет
+
+int a = int.Parse(Console.ReadLine());
+
+int b = int.Parse(Console.ReadLine());
+
+if (a == b*b)
+
+{
+    Console.WriteLine("да");
+}
+else 
+{
+   Console.WriteLine("нет"); 
+}
