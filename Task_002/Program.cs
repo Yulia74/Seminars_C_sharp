d2f2d395a5426eaebdd8b4e78c75f7ba6e67d585
[Yulia Lebedeva] <yl.75@mail.ru>
2022-11-28 22:56:29 +0500

    Task 002 is done

---
+++ Task_002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая будеть выдавать название дня
+// недели по заданному номеру: 3 -> среда, 5 -> пятница 
+
+int n = int.Parse(Console.ReadLine());
+
+if(n<1 | n>7)
+{
+    Console.WriteLine("Неправильно ввели число");
+}
+if(n==1)
+{
+    Console.WriteLine("Понедельник"); 
+}
+if(n==2)
+{
+    Console.WriteLine("Вторник"); 
+}
+if(n==3)
+{
+    Console.WriteLine("Среда"); 
+}
+if(n==4)
+{
+    Console.WriteLine("Четверг"); 
+}
+if(n==5)
+{
+    Console.WriteLine("Пятница"); 
+}
+if(n==6)
+{
+    Console.WriteLine("Суббота"); 
+}
+if(n==7)
+{
+    Console.WriteLine("Воскресенье"); 
+}
+
