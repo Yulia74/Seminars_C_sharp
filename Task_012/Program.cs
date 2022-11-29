b1a1f7826879cd96afd94197d2b6474304c8447e
[Yulia Lebedeva] <yl.75@mail.ru>
2022-11-29 22:04:37 +0500

    Task 012 is done

---
+++ Task_012/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+// Например: 34, 5 -> не кратно, остаток 4 
+//           16, 4 -> кратно
+
+Console.WriteLine("Enter Number1");
+
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number2");
+
+int number2 = int.Parse(Console.ReadLine());
+
+int remains = number1 % number2; 
+
+if(remains == 0)
+{
+    Console.WriteLine("number1 is a multiple of the number2");
+}
+else
+{
+    Console.WriteLine($"non-multiple, remainder after division {remains}");
+}
