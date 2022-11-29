1f7b5e839a841ce7bf1ef994419531b5249328a6
[Yulia Lebedeva] <yl.75@mail.ru>
2022-11-29 23:35:24 +0500

    Task 013 is done

---
+++ Task_013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+// Например: 14 -> нет,   46 -> нет,   161 -> да
+
+Console.WriteLine("Enter number");
+
+int number = int.Parse(Console.ReadLine());
+
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+  Console.WriteLine("the entered number is a multiple of 7 & 23");
+}
+else
+{
+  Console.WriteLine("the entered number is not a multiple of 7 & 23");
+}
+
