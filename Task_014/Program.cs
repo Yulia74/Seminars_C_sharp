039f0956f30ac41fcd745b3b2bfa274ced067149
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-01 00:44:02 +0500

    Task 014 is done

---
+++ Task_014/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+// Например: 5, 25 -> да,    -4, 16 -> да,       25, 5 -> да,       8,9 -> нет
+
+Console.Write("Enter first number ");
+
+// int number1 = int.Parse(Console.ReadLine()); 
+
+bool isParsed = int.TryParse(Console.ReadLine(),out int number1); // заменяет строку 7
+
+if(!isParsed)
+{
+    Console.WriteLine("what you entered is not a number");
+    return;
+}
+Console.Write("Enter second number ");
+
+bool isParsed1= int.TryParse(Console.ReadLine(),out int number2);
+
+if(!isParsed1)
+{
+    Console.WriteLine("what you entered is not a number");
+    return;
+}
+if(number2 == number1 * number1 || number1 == number2 * number2)
+{
+     Console.WriteLine("One of the entered numbers is the square of the other");
+}
+else
+{
+     Console.WriteLine("One of the entered numbers is not the square of the other");  
+}
