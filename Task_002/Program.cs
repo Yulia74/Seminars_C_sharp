3f2a15806ab59c533a64747774750c79ee230747
0xc000007b <80596788+0xc0000007b@users.noreply.github.com>
2022-12-01 14:34:18 +0300

    Update Program.cs

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -3,36 +3,6 @@
 
 int n = int.Parse(Console.ReadLine());
 
-if(n<1 | n>7)
-{
-    Console.WriteLine("Неправильно ввели число");
-}
-if(n==1)
-{
-    Console.WriteLine("Понедельник"); 
-}
-if(n==2)
-{
-    Console.WriteLine("Вторник"); 
-}
-if(n==3)
-{
-    Console.WriteLine("Среда"); 
-}
-if(n==4)
-{
-    Console.WriteLine("Четверг"); 
-}
-if(n==5)
-{
-    Console.WriteLine("Пятница"); 
-}
-if(n==6)
-{
-    Console.WriteLine("Суббота"); 
-}
-if(n==7)
-{
-    Console.WriteLine("Воскресенье"); 
-}
-
+if(n == 6 && n == 7) Console.WriteLine("Выходные"); 
+else if(n < 1 && n > 7) Console.WriteLine("Неправильно ввели число");
+else Concole.Write.Line("That day is not exits"):
