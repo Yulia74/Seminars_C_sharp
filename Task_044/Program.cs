14056227b9874e624c9bdb367f9036ccc97df41e
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-14 00:30:51 +0500

    Task_044 is done

---
+++ Task_044/Program.cs
@@ -0,0 +1,30 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Еnter a positive integer: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = Fibonacci(number);
+
+PrintArray(array);
+
+int[] Fibonacci(int number)
+{
+    int[] array = new int[number];
+    array[0] = 0;
+    array[1] = 1;
+
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(@"[{0}]", string.Join(",", array));
+}
