24e1575f1a1e3973de8b2f84a5e68877b5c86e43
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-16 22:50:44 +0500

    Task_045 is done

---
+++ Task_045/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+int[] array = CreateArrayRndInt(0, 10, 8);
+
+PrintArray(array);
+
+int[] newArray = CopyArray(array);
+
+PrintArray(newArray);
+
+
+
+
+int[] CreateArrayRndInt(int min, int max, int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(@"[{0}]", string.Join("; ", array));
+}
+
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+
