4a5d29c5b0a767f1fdb527a3d538eeb2b6d48f5a
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-23 23:14:07 +0500

    Task_063 is done

---
+++ Task_063/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от 1 до N.
+// N = 5    ->   "1, 2, 3, 4, 5"
+// N = 6    ->   "1, 2, 3, 4, 5, 6"
+
+PrintDigits(1, 5);
+
+Console.WriteLine();
+
+
+void PrintDigits(int a, int m)
+{
+    Console.Write(a + ", ");
+    if (a < m)
+        PrintDigits(a + 1, m);
+}
