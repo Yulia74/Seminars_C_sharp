ca8ecc27699d5aab911ba000f32c47ca5861eaa1
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-24 23:17:31 +0500

    Task_04 is done

---
+++ Home_work_01/Task_04/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел:
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Enter the first number");
+
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the third number");
+
+int n3 = int.Parse(Console.ReadLine());
+
+if (n1 > n2 & n1 > n3)
+{
+    Console.WriteLine($"max = {n1}");
+}
+else if (n2 > n1 & n2 > n3)
+{
+    Console.WriteLine($"max = {n2}");
+}
+
+else if (n3 > n1 & n3 > n2)
+{
+    Console.WriteLine($"max = {n3}");
+}
+else
+{
+Console.WriteLine("Numbers are equal");
+}
