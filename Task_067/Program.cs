ec26c00c0c6326951aa0f89186a3a2913bc90a17
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-24 21:19:30 +0500

    Task_067 is done

---
+++ Task_067/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать
+// сумму его цифр.
+// 453 -> 12
+//  45 -> 9
+
+
+Console.Write("Enter the first number n:  ");
+
+int n = int.Parse(Console.ReadLine());
+
+
+int SumDigits(int n)
+{
+    if (n > 0) return 
+    SumDigits(n / 10) + n % 10;
+    else return 0;
+}
+
+ Console.WriteLine($"Сумма цифр в числе: {SumDigits(n)}");
+ Console.WriteLine();
